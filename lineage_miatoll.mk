cae10a8aba2b207e325bffcbe13ece57733c67b8
UdayBindal2312 <udaybindal2312@gmail.com>
2022-09-20 13:02:03 +0530

    [miatoll] LOS Bringup

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -1,26 +1,14 @@
-# Copyright (C) 2020 Fluid
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (C) 2022 
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -30,7 +18,7 @@ CUSTOM_DEVICE := miatoll
 
 # Build Flags 
 BUILD_USERNAME := iTS_UDAY_404
-BUILD_HOSTNAME := AOSP
+BUILD_HOSTNAME := LineageOS
 TARGET_SUPPORTS_CALL_RECORDING := true
 DEVICE_MAINTAINER := iTS_UDAY_404
 SHIP_ANX := false
