558643205f919a7c894c65b5e486bb83354846ab
UdayBindal2312 <udaybindal2312@gmail.com>
2022-09-20 13:38:39 +0530

    [miatoll] Rice Bringup

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -1,26 +1,15 @@
-# Copyright (C) 2020 Fluid
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (C) 2022 
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+SUSHI_BOOTANIMATION := 1080
 
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -30,11 +19,21 @@ CUSTOM_DEVICE := miatoll
 
 # Build Flags 
 BUILD_USERNAME := iTS_UDAY_404
-BUILD_HOSTNAME := AOSP
+BUILD_HOSTNAME := RiceOS
 TARGET_SUPPORTS_CALL_RECORDING := true
 DEVICE_MAINTAINER := iTS_UDAY_404
 SHIP_ANX := false
-WITH_GAPPS := false
+
+# RiceDroid Flags
+RICE_DEVICE := miatoll
+RICE_CHIPSET := atoll
+RICE_OFFICIAL := false
+RICE_MAINTAINER := iTS_UDAY_404
+WITH_GMS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := false
 
 # FP at Brunch
 PRODUCT_BUILD_PROP_OVERRIDES += \
