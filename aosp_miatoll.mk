9fd93d63b22fdc5a1db6927a5df7e6283fd3ef28
UdayBindal2312 <udaybindal2312@gmail.com>
2022-10-12 15:45:34 +0530

    miatoll: Rearranging Some Flags
    Signed-off-by: UdayBindal2312 <udaybindal2312@gmail.com>

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -1,19 +1,13 @@
-# Copyright (C) 2020 Fluid
+# Copyright (C) 2022 iTS_UDAY404
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Inherit from products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit From ROM & Device 
+#$(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation 
 TARGET_BOOT_ANIMATION_RES := 1080
